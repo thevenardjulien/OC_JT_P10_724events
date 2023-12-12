dee1e337a981362da4e6bda11095ebd485d1f0b7
THEVENARD Julien <thevenard.julien@gmail.com>
2023-12-12 23:39:09 +0100

    Fix Slider Key Render

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -27,36 +27,35 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {event &&
-                byDateDesc.map((_, radioIdx) => (
-                  <input
-                    type="radio"
-                    name="radio-button"
-                    checked={index === radioIdx}
-                    onChange={() => {}}
-                  />
-                ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={event.title}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => {}}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
