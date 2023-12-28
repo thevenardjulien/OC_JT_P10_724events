db971082ae3602a23f5d615e9455dbc5f0623cb7
THEVENARD Julien <thevenard.julien@gmail.com>
2023-12-28 08:50:49 +0100

    Icon tests

--- src/components/Icon/index.test.js
+++ src/components/Icon/index.test.js
@@ -6,17 +6,17 @@ describe("Icon component", () => {
   describe("When a icon is created with name twitch", () => {
     it("the icon contain this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
       render(<Icon name="twitch" />);
-      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+      expect(md5(screen.getByTestId("twitch-icon").getAttribute("d"))).toEqual(
         "327fbc38c8e878259c3ec35ef231517a"
       );
     });
   });
   describe("When a icon is created with name facebook", () => {
-    // it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
-    //   render(<Icon name="facebook" />);
-    //   expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
-    //     "bbea4c9e40773b969fdb6e406059f853"
-    //   );
-    // });
+    it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
+      render(<Icon name="facebook" />);
+      expect(
+        md5(screen.getByTestId("facebook-icon").getAttribute("d"))
+      ).toEqual("bbea4c9e40773b969fdb6e406059f853");
+    });
   });
 });
